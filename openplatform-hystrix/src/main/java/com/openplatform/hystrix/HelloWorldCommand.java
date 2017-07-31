5b18050e10644df7d62dc7b642bd105d3eac985a
liuheng <liuheng@iboxpay.com>
2017-07-31 11:17:02 +0800

    ftp模块

--- openplatform-hystrix/src/main/java/com/openplatform/hystrix/HelloWorldCommand.java
+++ openplatform-hystrix/src/main/java/com/openplatform/hystrix/HelloWorldCommand.java
@@ -25,15 +25,16 @@ public class HelloWorldCommand extends HystrixCommand<String> {
 
     @Override
     protected String run() {
+        String threadName = Thread.currentThread().getName();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(name + " start = " + formatter.format(new Date()));
+        System.out.println(threadName + " " + name + " start = " + formatter.format(new Date()));
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.MILLISECONDS.sleep(3);
         } catch (Exception e) {
-            System.out.println(name + " exception");
+            System.out.println(threadName + " " + name + " exception");
             e.printStackTrace();
-        }finally {
-            System.out.println(name + " end = " + formatter.format(new Date()));
+        } finally {
+            System.out.println(threadName + " " + name + " end = " + formatter.format(new Date()));
         }
         // 依赖逻辑封装在run()方法中
         return "Hello " + name + " thread:" + Thread.currentThread().getName();
@@ -41,21 +42,21 @@ public class HelloWorldCommand extends HystrixCommand<String> {
 
     @Override
     protected String getFallback() {
-        return "fall back";
+        return Thread.currentThread().getName() + " fall back";
     }
 
     //调用实例
     public static void main(String[] args) throws Exception {
         //每个Command对象只能调用一次,不可以重复调用,
         //重复调用对应异常信息:This instance can only be executed once. Please instantiate a new instance.
-        HelloWorldCommand helloWorldCommand = new HelloWorldCommand("Synchronous-hystrix");
+        HelloWorldCommand helloWorldCommand1 = new HelloWorldCommand("Synchronous-hystrix");
         //使用execute()同步调用代码,效果等同于:helloWorldCommand.queue().get();
-        String result = helloWorldCommand.execute();
+        String result = helloWorldCommand1.execute();
         System.out.println("sync result=" + result);
 
-        helloWorldCommand = new HelloWorldCommand("Asynchronous-hystrix");
+        HelloWorldCommand helloWorldCommand2 = new HelloWorldCommand("Asynchronous-hystrix");
         //异步调用,可自由控制获取结果时机,
-        Future<String> future = helloWorldCommand.queue();
+        Future<String> future = helloWorldCommand2.queue();
         //get操作不能超过command定义的超时时间,默认:1秒
         result = future.get(100, TimeUnit.MILLISECONDS);
         System.out.println("async result=" + result);
